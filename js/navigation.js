99c6c34fa03856af9857a236f7511bdd49007bbb
Murat Seyhan <muratseyhan5@gmail.com>
2016-10-21 10:55:07 +0200

    Clickable logo fix.

---
+++ js/navigation.js
@@ -0,0 +1,22 @@
+angular.module('viewCustom').factory('navigation', ['$location', '$window', function($location, $window) {
+  
+  return {
+
+    navigateTo: function(url) {      
+      if(url === "")
+        ctrl.navigateToHomePage();
+      else 
+        $window.open(url, '_blank');
+    },
+
+    navigateToHomePage: function() {
+      var params = $location.search();
+      var vid = params.vid || "NUI";
+      var lang = params.lang || "da_DK";
+      var baseUrl = $location.absUrl().split('?')[0];
+      $window.location.href = baseUrl + '?vid=' + vid + '&lang=' + lang;
+    }
+
+  }
+  
+}]);
---
+++ js/prmLogoAfter.js
@@ -0,0 +1,20 @@
+// Clickable logo.
+
+angular.module('viewCustom').controller('prmLogoAfterController', ['navigation', function (navigation) {
+  var ctrl = this;
+  
+  ctrl.$onInit = function () {
+    ctrl.navigation = navigation;
+  };
+  
+  ctrl.getIconLink = function () {
+    return ctrl.parentCtrl.iconLink;
+  };
+
+}]);
+  
+angular.module('viewCustom').component('prmLogoAfter',{
+  bindings: { parentCtrl: '<' },
+  controller: 'prmLogoAfterController',
+  templateUrl: 'custom/NUI/html/prmLogoAfter.html'  
+});
