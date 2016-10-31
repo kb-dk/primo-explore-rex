6f3d1c16bbd9fdf441de3223551e58e0a3a3b43e
Murat Seyhan <muratseyhan5@gmail.com>
2016-10-31 14:37:13 +0100

    Added comments.

--- js/prmPersonalInfoAfter.js
+++ js/prmPersonalInfoAfter.js
@@ -1,8 +1,9 @@
 angular.module('viewCustom').controller('prmPersonalInfoAfterController', ['navigation', function (navigation) {
   var ctrl = this;
 
-  
   ctrl.$onInit = function () {
+    // Replacing the functionality of the 'Edit' button.
+    // It now navigates the user to the corresponding editing page for the user database.
     ctrl.parentCtrl.editDetails = () => { navigation.navigateTo('https://user.kb.dk/user/edit')};
   }
 
