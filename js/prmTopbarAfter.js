33ab75448cb1104976771929aa96984672eaf09f
Murat Seyhan <muratseyhan5@gmail.com>
2016-11-15 11:19:04 +0100

    Info bar hiding mechanism refined.

--- js/prmTopbarAfter.js
+++ js/prmTopbarAfter.js
@@ -2,26 +2,34 @@ angular.module('viewCustom').controller('prmTopbarAfterController', ['$scope', '
   var ctrl = this;
 
   ctrl.$onInit = function() {
-    
+
     $translate('nui.message.dismiss')
       .then(function(translatedValue) {
-        
+
         // Check if there is a message to be displayed with the info bar
         if (translatedValue !== "dismiss") {
           // If so, display
           $mdToast.show({
-            hideDelay: 300000,
+            hideDelay: 100000,
             position: 'top',
             controller: 'infoBarController',
             templateUrl: 'custom/' + globalViewName + '/html/infoBar.html'
-          });
-
-          // Shift the topbar below to avoid it to be hidden by the info bar.
+          }).then(shiftTopBarUp).catch(shiftTopBarUp);
+        
+          // Shift the topbar down to avoid overlap.
           $document.find('prm-topbar').addClass('shifted-topbar');
+
         }
       });
 
   };
+
+  // Shift the topbar back into its original position.
+  function shiftTopBarUp() {
+    return angular.element(document).find('prm-topbar').removeClass('shifted-topbar');
+  }
+
+
 
 }]);
 
