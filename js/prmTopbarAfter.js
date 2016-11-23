96a4ac092a2b00bbc3f09fb7cab89281c8255f5a
Murat Seyhan <muratseyhan5@gmail.com>
2016-11-23 16:49:40 +0100

    Info bar bug fix.

--- js/prmTopbarAfter.js
+++ js/prmTopbarAfter.js
@@ -3,25 +3,24 @@ angular.module('viewCustom').controller('prmTopbarAfterController', ['$scope', '
 
   ctrl.$onInit = function() {
 
-    // $translate('nui.message.announcement')
-    //   .then(function(translatedValue) {
-    //     // Check if there is a message to be displayed with the info bar.
-    //     // translatedValue is initialized to 'announcement' in the absence of a matching entry. '
-    //     if (translatedValue !== 'announcement') {
-    //       // If so, display
-    //       $mdToast.show({
-    //         // Timeout duration in msecs. false implies no timeout.
-    //         hideDelay: false, 
-    //         position: 'top',
-    //         controller: 'infoBarController',
-    //         templateUrl: 'custom/' + globalViewName + '/html/infoBar.html'
-    //       }).then(shiftTopBarUp).catch(shiftTopBarUp);
-        
-    //       // Shift the topbar down to avoid overlapping.
-    //       $element.parent().addClass('shifted-topbar');
+    $translate('nui.message.announcement')
+      .then(function(translatedValue) {
+        // Check if there is a message to be displayed with the info bar.
+        // translatedValue is initialized to 'announcement' in the absence of a matching entry. '
+        if (translatedValue !== 'announcement') {
+          // If so, display
+          $mdToast.show({
+            // Timeout duration in msecs. false implies no timeout.
+            hideDelay: false, 
+            position: 'top',
+            controller: 'infoBarController',
+            templateUrl: 'custom/' + globalViewName + '/html/infoBar.html',
+          }).then(shiftTopBarUp).catch(shiftTopBarUp);
 
-    //     }
-    //   });
+          // Shift the topbar down to avoid overlapping.
+          $element.parent().addClass('shifted-topbar');
+        }
+      });
 
   };
 
