dbf8c9d9421fbe9a5806dbf5c809ab2788748c1d
Murat Seyhan <muratseyhan5@gmail.com>
2016-12-15 17:09:17 +0100

    Search tips refactored.

--- js/prmSearchBarAfter.js
+++ js/prmSearchBarAfter.js
@@ -1,39 +1,10 @@
-// TODO: Decide if the logic needs to be extracted out into a service.
-angular.module('viewCustom').controller('prmSearchBarAfterController', ['$element', '$mdDialog', '$locale', function($element, $mdDialog, $locale) {
+angular.module('viewCustom').controller('prmSearchBarAfterController', ['$element', function($element) {
   var ctrl = this;
 
   ctrl.$postLink = function() {
-
     var container = angular.element($element.parent().children()[0].children[0]);
-    console.log($element.parent());
-
     container.append($element.children()[0]);
   };
-
-  ctrl.showSearchTips = function(ev) {
-    $mdDialog.show({
-        controller: DialogController,
-        templateUrl: 'custom/' + globalViewName + '/html/searchTips_' + $locale.localeID +'.html',
-        parent: angular.element(document.body),
-        targetEvent: ev,
-        clickOutsideToClose: true,
-        fullscreen: false // Only for -xs, -sm breakpoints.
-      });
-  };
-
-  function DialogController($scope, $mdDialog) {
-    $scope.hide = function() {
-      $mdDialog.hide();
-    };
-
-    $scope.cancel = function() {
-      $mdDialog.cancel();
-    };
-
-    $scope.answer = function(answer) {
-      $mdDialog.hide(answer);
-    };
-  }
 
 }]);
 
@@ -41,16 +12,6 @@ angular.module('viewCustom').component('prmSearchBarAfter', {
   bindings: {
     parentCtrl: '<'
   },
-  // templateUrl: 'custom/' + globalViewName + '/html/prmSearchBarAfter.html',
-  template: `
-<div flex hide show-gt-sm layout-align='end center' layout='row' >
-<rex-search-tip>
-  <md-button class='md-icon-button' aria-label="{{'nui.search.search_tips' | translate}}" ng-click='$ctrl.showSearchTips($event)'>
-    <md-tooltip md-direction='bottom'>{{'nui.search.search_tips' | translate}}</md-tooltip>
-    <md-icon md-svg-icon='primo-ui:help-circle-outline'></md-icon>
-  </md-button>
-</rex-search-tip>
-</div>
-  `,
+  templateUrl: 'custom/' + globalViewName + '/html/prmSearchBarAfter.html',
   controller: 'prmSearchBarAfterController',
 });
---
+++ js/searchTips.js
@@ -0,0 +1,37 @@
+angular.module('viewCustom').controller('searchTipsContoller', ['navigation', '$mdDialog', '$locale', function(navigation, $mdDialog, $locale) {
+  var ctrl = this;
+
+  ctrl.showSearchTips = function(ev) {
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: 'custom/' + globalViewName + '/html/searchTips_' + $locale.localeID + '.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose: true,
+      fullscreen: false // Only for -xs, -sm breakpoints.
+    });
+  };
+
+  function DialogController($scope, $mdDialog) {
+    $scope.hide = function() {
+      $mdDialog.hide();
+    };
+
+    $scope.cancel = function() {
+      $mdDialog.cancel();
+    };
+
+    $scope.answer = function(answer) {
+      $mdDialog.hide(answer);
+    };
+  }
+
+}]);
+
+angular.module('viewCustom').component('rexSearchTips', {
+  bindings: {
+    parentCtrl: '<'
+  },
+  controller: 'searchTipsContoller',
+  templateUrl: 'custom/' + globalViewName + '/html/searchTips.html'
+});
