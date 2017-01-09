0b658177067f9da9e5dab99ff3aa630c3d51d88b
Murat Seyhan <muratseyhan5@gmail.com>
2017-01-09 16:18:09 +0100

    Altmetrics component improved, unit tests fixed.

--- js/altmetrics.js
+++ js/altmetrics.js
@@ -1,39 +1,43 @@
 angular.module('viewCustom').controller('altmetricsController', [
   'scriptLoader',
-  function(scriptLoader) {
+  '$window',
+  function(scriptLoader, $window) {
     var ctrl = this;
 
     ctrl.$onInit = function() {
-      ctrl.doi = ctrl.parentCtrl.parentCtrl.item.pnx.addata.doi[0];
-      console.log(ctrl.doi);
-      ctrl.loadBadge();
+      try {
+        ctrl.doi = ctrl.parentCtrl.parentCtrl.item.pnx.addata.doi[0];
+        ctrl.loadBadge().catch((e) => console.log(e.message));
+      } catch (e) {
+        console.log(e.message);
+      }
     };
 
     /**
-     * 
+     *  Method to load the altmetrics badge.
      *  @return {Promise} A Promise to be fulfilled if the badge is loaded, 
      *  and to be rejected if it could not be loaded.
      */
     ctrl.loadBadge = function() {
-      return new Promise(function(resolve, reject) {
-        if (ctrl.doi) {
-          scriptLoader.load('https://d1bxh8uas1mnw7.cloudfront.net/assets/embed.js');
-          resolve();
-        } else {
-          reject('Altmetrics badge cannot be loaded as no DOI is present.');
-        }
-      });
+      if (ctrl.doi) {
+        return scriptLoader.load('https://d1bxh8uas1mnw7.cloudfront.net/assets/embed.js');
+      } else {
+        return Promise.reject('Altmetrics badge cannot be loaded as no DOI is present.');
+      }
     };
 
     ctrl.$onDestroy = function() {
-      // TODO: Remove any other JS or CSS files that are be loaded.
+      // TODO: Remove any other JS or CSS files that are loaded.
       // TODO: Remove the added properties and global variables. 
+      // if (_altmetric) _altmetric = null;
+      // if ($window._altmetric) $window._altmetric = null;
+
       scriptLoader.unload('https://d1bxh8uas1mnw7.cloudfront.net/assets/embed.js', 'js');
-      // scriptLoader.unload('openingHoursStyles_min.js', 'css');
+      scriptLoader.unload('https://d1bxh8uas1mnw7.cloudfront.net/assets/altmetric_badges-8f271adb184c21cc5169a7f67f7fe5ab.js', 'js');
+      scriptLoader.unload('https://d1bxh8uas1mnw7.cloudfront.net/assets/embed-2c47105b6381604898bbf8ae8a680350.css', 'css');
 
-      console.log('Opening hours widget destroyed!.');
+      console.log('Altmetrics badge is destroyed!.');
     };
-
 
   }
 ]);
--- test/unit/specs/altmetrics.spec.js
+++ test/unit/specs/altmetrics.spec.js
@@ -1,47 +1,93 @@
 describe('altmetricsController,', function() {
-  var $httpBackend, altmetricsController;
+  var scriptLoader, altmetricsController, $compile, element, _altmetrics, scope;
 
   beforeEach(module('viewCustom'));
 
-  beforeEach(inject(function(_$httpBackend_) {
-    $httpBackend = _$httpBackend_;
+  beforeEach(inject(function(_scriptLoader_, $rootScope, _$compile_) {
+    scriptLoader = _scriptLoader_;
+    spyOn(scriptLoader, 'load').and.callThrough();
+    $compile = _$compile_;
+    scope = $rootScope.$new();
   }));
 
-  beforeEach(inject(function($contorller) {
-    altmetricsController = $controller('altmetricsController');
-  }));
+  it('when a DOI is present, should send a request to load the altmetrics badge.', function(done) {
+    var doi = '10.1007/BF01386390';
 
-  describe('when a DOI is not present,', function() {
-    beforeEach(inject(function() {
-      altmetricsController.doi = '10.1038/nature.2012.9872';
-    }));
+    scope.parentCtrl = {
+      parentCtrl: {
+        item: {
+          pnx: {
+            addata: {
+              doi: [doi]
+            }
+          }
+        }
+      }
+    };
 
-    it('should load not the altmetrics badge.', function(done) {
-      altmetricsController.loadBadge().then(function() {
-        // Should not be executed.
-        expect(true).toEqual(false);        
-      }).catch(function() {}).then(done);
-    });
+    element = angular.element('<rex-altmetrics parent-ctrl="parentCtrl"></rex-altmetrics>');
+    $compile(element)(scope);
+    altmetricsController = element.controller('rexAltmetrics');
 
+    scope.$digest();
+    expect(altmetricsController.doi).toEqual(doi);
+    expect(scriptLoader.load).toHaveBeenCalledWith('https://d1bxh8uas1mnw7.cloudfront.net/assets/embed.js');
+
+    done();
   });
 
+  it('when a DOI is present, should not send a request to load the altmetrics badge.', function(done) {
+    element = angular.element('<rex-altmetrics></rex-altmetrics>');
+    $compile(element)(scope);
+    altmetricsController = element.controller('rexAltmetrics');
 
-  describe('when a DOI is present,', function() {
-    beforeEach(inject(function() {
-      altmetricsController.doi = '10.1038/nature.2012.9872';
-    }));
+    scope.$digest();
+    expect(altmetricsController.doi).toBeUndefined();
+    expect(scriptLoader.load).not.toHaveBeenCalled();
 
-    it('should load the altmetrics badge.', function(done) {
-      altmetricsController.loadBadge().catch(function() {
-        // Should not be executed.
-        expect(true).toEqual(false);        
-      }).then(done);
-
-      $httpBackend.expectGet('https://d1bxh8uas1mnw7.cloudfront.net/assets/embed.js').respond(200);
-      // Another expectGet is needed here for the retrieval of the metrics.
-      $httpBackend.flush();
-    });
-
+    done();
   });
 
 });
+
+
+// describe('altmetricsController,', function() {
+//   var scriptLoader, altmetricsController, createController;
+
+//   beforeEach(module('viewCustom'));
+
+//   beforeEach(inject(function(_scriptLoader_, $rootScope, $componentController) {
+//     scriptLoader = _scriptLoader_;
+//     spyOn(scriptLoader, 'load').and.returnValue(Promise.resolve());
+
+//     scope = $rootScope.$new();
+//     altmetricsController = $componentController('rexAltmetrics', scope, {});
+
+//   }));
+
+//   it('when a DOI is present, should send a request to load the altmetrics badge.', function(done) {
+
+//     altmetricsController.doi = '10.1007/BF01386390';
+
+//     altmetricsController.loadBadge().then(function() {
+//       expect(scriptLoader.load).toHaveBeenCalledWith('https://d1bxh8uas1mnw7.cloudfront.net/assets/embed.js');
+//     }).catch(function(e) {
+//       // Should not be executed
+//       expect(true).toEqual(false);
+//     }).then(done);
+//   });
+
+//   it('when a DOI is present, should not send a request to load the altmetrics badge.', function(done) {
+
+//     altmetricsController.doi = undefined;
+
+//     altmetricsController.loadBadge().then(function() {
+//       // Should not be executed
+//       expect(true).toEqual(false);
+//     }).catch(function(e) {
+//       expect(scriptLoader.load).not.toHaveBeenCalled();
+//     }).then(done);
+
+//   });
+
+// });
