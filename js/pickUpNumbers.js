431dd7b76c757732a2d5b5fd1eba08a80c0e2d7b
Murat Seyhan <muratseyhan5@gmail.com>
2017-01-17 11:38:13 +0100

    Further refactoring for pick-up number retrieval.

--- js/pickUpNumbers.js
+++ js/pickUpNumbers.js
@@ -1,6 +1,6 @@
 /** 
-  * Service to insert the pick up numbers for the requested items.
-  */
+ * Service to insert the pick up numbers for the requested items.
+ */
 
 // TODO: Need to be optimized! The current way we utilize this service yields two requests per record.
 angular.module('viewCustom').service('pickUpNumbers', [
@@ -13,11 +13,11 @@ angular.module('viewCustom').service('pickUpNumbers', [
       $location.absUrl().split("/primo-explore/")[0] + "/cgi-bin/";
     let pickUpNumbersForIds = {};
 
-    // Retrieving the pick up numbers from the associated service. 
+    // Retrieves the pick up numbers from the associated service. 
     // Request URL may look like the following.
     // https://rex.kb.dk/cgi-bin/get_pickup_number_text?z37_rec_key=0078814230000100001
     // 'https://rex-test.kb.dk/cgi-bin/get_pickup_number_title_kgl?z370_rec_key=000000371
-    function retrieve(requestId) {
+    function retrievePickUpNumber(requestId) {
 
       let serviceURL = serviceBaseUrl;
       let titleMatch = requestId.match(/^TITLE([0-9]*)/);
@@ -31,6 +31,55 @@ angular.module('viewCustom').service('pickUpNumbers', [
       return $http.get(serviceURL);
     };
 
+    // Returns an array of objects with request related data.
+    function composeRequestObjects(targetElements, requests) {
+      return requests.map((request) => {
+        let regex = request.requestId + "$";
+        return {
+          element: Array.prototype.find.call(targetElements, (target) => {
+            return target.textContent.match(new RegExp(regex))
+          }),
+          id: request.requestId,
+          expandedDisplay: request.expandedDisplay
+        }
+      });
+    };
+
+    // Inserts the pickup number for given request.
+    function insertForRequest(request) {
+      if (!request.element) return;
+
+      if (pickUpNumbersForIds[request.id]) {
+        // If the pick-up number for the request is already known, use it.  
+        replaceIdText(request, pickUpNumbersForIds[request.id]);
+      } else if (request.expandedDisplay.find((field) => field.label === "request.holds.end_hold_date")) {
+        // Else, if it has a hold deadline, retrieve and insert the pick-up number.
+        // The requested item can only have a pick up number if it has a hold deadline.
+        retrievePickUpNumber(request.id).then((response) => {
+          // Insert the pick-up number text.
+          let pickUpNumber = response.data.split(/<body>|<\/body>/)[1];
+          pickUpNumbersForIds[request.id] = pickUpNumber;
+          replaceIdText(request, pickUpNumber);
+        }).catch(() => {
+          removeIdText(request);
+          console.log('REX: Could not retrieve the pick up number.');
+        });
+
+      } else {
+        // Else, remove the request ID from the view.
+        removeIdText(request);
+      }
+    }
+
+    // Replaces the request ID text with the given string.
+    function replaceIdText(request, text) {
+      request.element.textContent = request.element.textContent.replace("-" + request.id, text);
+    };
+
+    // Removes the request ID.
+    function removeIdText(request) {
+      replaceIdText(request, "");
+    };
 
     /** 
      *  Retrieves and inserts the pick-up numbers.
@@ -44,59 +93,12 @@ angular.module('viewCustom').service('pickUpNumbers', [
      *    targetContainer.
      */
     this.insertPickUpNumbers = (targetContainer, requests, selector) => {
-
-      var targetElements = selector(targetContainer);
-
-      requests.forEach(function(requestItem) {
-        var requestId = requestItem.requestId;
-        var regex = requestId + "$"
-
-        // The DOM element containing the request ID of the item.
-        var matchedElement = Array.prototype.find.call(targetElements, (target) => {
-          return target.textContent.match(new RegExp(regex))
-        });
-
-        // If the request ID cannot be found, skip the check for the request.
-        if (!matchedElement) return;
-
-        if (pickUpNumbersForIds[requestId]) {
-        // If the pick-up number for the request is already known, use it.  
-          replaceIdText(pickUpNumbersForIds[requestId]);
-        } else if (requestItem.expandedDisplay.find((field) => field.label === "request.holds.end_hold_date")) {
-          // Else, if it has a hold deadline, retrieve and insert the pick-up number.
-          // The requested item can only have a pick up number if it has a hold deadline.
-          retrieve(requestId).then(insertPickUpNumber).catch(logError).catch(removeId);
-        } else {
-          // Else, remove the request ID from the view.
-          removeId();
-        }
-
-        // Insert the pick-up number.
-        function insertPickUpNumber(response) {
-          let pickUpNumber = response.data.split(/<body>|<\/body>/)[1];
-          pickUpNumbersForIds[requestId] = pickUpNumber;
-          replaceIdText(pickUpNumber);
-        };
-
-        // Log the request error.
-        function logError(response) {
-          console.log('REX: Could not retrieve the pick up number.');
-          return Promise.reject();
-        };
-
-        // Replaces the request ID text with the given string.
-        function replaceIdText(pickUpNumber) {
-          matchedElement.textContent = matchedElement.textContent.replace("-" + requestId, pickUpNumber);
-        };
-
-        // Removes the request ID.
-        function removeId() {
-          replaceIdText("");
-        };
-
+      let targetElements = selector(targetContainer);
+      let requestObjects = composeRequestObjects(targetElements, requests);
+      requestObjects.forEach((request) => {
+        insertForRequest(request);
       });
-
-    };
+    }
 
   }
 ]);
