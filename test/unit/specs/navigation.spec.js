83f2d821aee6be1773f755c291c53350a607e78b
Murat Seyhan <muratseyhan5@gmail.com>
2017-02-27 13:58:32 +0100

    Navigation service unit test added.

--- test/unit/specs/navigation.spec.js
+++ test/unit/specs/navigation.spec.js
@@ -1,41 +1,68 @@
-// describe('navigation service,', () => {
-//   let $location, $window, navigation;
+describe('navigation service,', () => {
+  let $location, $window, navigation, dummyPath;
 
-//   beforeEach(module('viewCustom'));
+  beforeEach(module('viewCustom'));
 
-//   beforeEach(inject((_$location_) => {
-//     $location = _$location_;
+  // Mocking $window service.
+  beforeEach(function() {
 
+    dummyPath = 'https://rex.kb.dk/primo-explore/a/dummy/path/within/the/app?vid=NUI&lang=da_DK';
 
-//     spyOn($location, 'search').and.returnValue({
-//       lang: 'da_DK',
-//       vid: 'NUI'
-//     });
+    module(function($provide) {
+      
+      $provide.service('$window', function() {
+        return {
+          location : {
+            href: dummyPath
+          },
+          open: () => {}
+        };
 
-//     spyOn($location, 'absUrl').and.returnValue('https://rex.kb.dk/primo-explore/search?vid=NUI&lang=da_DK');
+      });
+    });
+  });
 
-//     $window = {
-//       location: {
-//         href: 'https://rex.kb.dk/primo-explore/search?vid=NUI&lang=da_DK'
-//       },
-//       open: () => {}
-//     }
+  beforeEach(inject((_$location_, _$window_) => {
+    $location = _$location_;
+    $window = _$window_;
 
-//     dump($window.location.href);
-//   }));
+    spyOn($location, 'search').and.returnValue({
+      lang: 'da_DK',
+      vid: 'NUI'
+    });
 
-//   beforeEach(inject((_navigation_) => {
-//     navigation = _navigation_;
-//   }));
+    spyOn($location, 'absUrl').and.returnValue(dummyPath);
+    
+    spyOn($window, 'open');
 
-//   describe('navigateToHomePage method,', () => {
+  }));
 
-//     it('should navigate to the home page.', () => {
-//       navigation.navigateToHomePage();
-//       expect($window.location.href).toEqual('https://rex.kb.dk/primo-explore/search?vid=NUI&lang=da_DK');
-//       dump($window.location.href);
-//     });
+  beforeEach(inject((_navigation_) => {
+    navigation = _navigation_;
+  }));
 
-//   });
+  describe('navigateToHomePage method,', () => {
 
-// });
+    it('should navigate to the home page.', () => {
+      navigation.navigateToHomePage();
+      // dump($window);
+      expect($window.location.href).toEqual('https://rex.kb.dk/primo-explore/search?vid=NUI&lang=da_DK');
+    });
+
+  });
+
+  describe('naviagateTo method', () => {
+
+    it('should navigate to the given URL if there is one.', () => {
+      navigation.navigateTo('https://example.com');
+      expect($window.open).toHaveBeenCalledWith('https://example.com', '_blank');
+    });
+
+    it('should navigate to the home page if no URL is given.', () => {
+      navigation.navigateTo();
+      expect($window.location.href).toEqual('https://rex.kb.dk/primo-explore/search?vid=NUI&lang=da_DK');
+    });
+
+  })
+
+});
