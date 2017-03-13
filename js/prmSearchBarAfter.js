d2d36d48d6527d41b074668f6f2f04254ea64d84
Murat Seyhan <muratseyhan5@gmail.com>
2017-03-13 11:12:42 +0100

    Search bar focus bug fix.

--- js/prmSearchBarAfter.js
+++ js/prmSearchBarAfter.js
@@ -13,9 +13,17 @@ class PrmSearchBarAfterController {
     let container = angular.element(parentElement.children()[0].children[0]);
     container.append(this.$element.children()[0]);
 
-    // Focus on the search bar.
-    parentElement[0].querySelector('#searchBar').focus();
+    let searchBarElement = parentElement[0].querySelector('#searchBar');
     
+    // Focus on the search bar, if it exists.
+    // Note that, when the language is changed, 
+    // the search bar is not available yet here.
+    // We can watch for the element and then focus on it,
+    // but it does not seem to worth it.
+    if (searchBarElement) {
+      searchBarElement.focus();
+    }
+
   };
 }
 
