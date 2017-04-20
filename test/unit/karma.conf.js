7593ff9376c53dc667e902a40541d3d80394e657
Murat Seyhan <muratseyhan5@gmail.com>
2017-04-20 10:52:21 +0200

    Firefox launcher added for unit testing.

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -81,8 +81,8 @@ module.exports = function (config) {
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ['Chromium'],
-        // browsers: ['Chrome'],
+        // browsers: ['Chromium'],
+        browsers: ['Firefox'],
 
 
         // Continuous Integration mode
